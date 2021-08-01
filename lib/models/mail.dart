7fe87db14f3a68f9fa221b701b3fd944cad30edd
suhas-kadu <kadusuhas72@gmail.com>
2021-08-01 18:20:34 +0530

    mail model added

---
+++ lib/models/mail.dart
@@ -0,0 +1,87 @@
+class Mail {
+  final String title;
+  final String subtitle;
+  final String imgUrl;
+  final String date;
+  final bool isStarred;
+
+  Mail(
+      {required this.title,
+      required this.subtitle,
+      required this.imgUrl,
+      required this.date,
+      this.isStarred = false});
+}
+
+List mails = <Mail>[
+  Mail(
+      title: "Matt\nDrawing classes",
+      subtitle: "Classes will resume from 20th August 2021",
+      imgUrl:
+          "https://images.unsplash.com/photo-1627346980717-02226f9ff7f6?ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDE4fHRvd0paRnNrcEdnfHxlbnwwfHx8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+      date: "6:12 PM",
+      isStarred: true),
+  Mail(
+    title: "Matt\nDrawing classes",
+    subtitle: "Classes will resume from 20th August 2021",
+    imgUrl:
+        "https://images.unsplash.com/photo-1627346980717-02226f9ff7f6?ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDE4fHRvd0paRnNrcEdnfHxlbnwwfHx8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+    date: "6:12 PM",
+  ),
+  Mail(
+    title: "Matt\nDrawing classes",
+    subtitle: "Classes will resume from 20th August 2021",
+    imgUrl:
+        "https://images.unsplash.com/photo-1627346980717-02226f9ff7f6?ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDE4fHRvd0paRnNrcEdnfHxlbnwwfHx8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+    date: "6:12 PM",
+  ),
+  Mail(
+      title: "Matt\nDrawing classes",
+      subtitle: "Classes will resume from 20th August 2021",
+      imgUrl:
+          "https://images.unsplash.com/photo-1627346980717-02226f9ff7f6?ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDE4fHRvd0paRnNrcEdnfHxlbnwwfHx8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+      date: "6:12 PM",
+      isStarred: true),
+  Mail(
+      title: "Matt\nDrawing classes",
+      subtitle: "Classes will resume from 20th August 2021",
+      imgUrl:
+          "https://images.unsplash.com/photo-1627346980717-02226f9ff7f6?ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDE4fHRvd0paRnNrcEdnfHxlbnwwfHx8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+      date: "6:12 PM",
+      isStarred: true),
+  Mail(
+    title: "Matt\nDrawing classes",
+    subtitle: "Classes will resume from 20th August 2021",
+    imgUrl:
+        "https://images.unsplash.com/photo-1627346980717-02226f9ff7f6?ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDE4fHRvd0paRnNrcEdnfHxlbnwwfHx8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+    date: "6:12 PM",
+  ),
+  Mail(
+    title: "Matt\nDrawing classes",
+    subtitle: "Classes will resume from 20th August 2021",
+    imgUrl:
+        "https://images.unsplash.com/photo-1627346980717-02226f9ff7f6?ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDE4fHRvd0paRnNrcEdnfHxlbnwwfHx8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+    date: "6:12 PM",
+  ),
+  Mail(
+      title: "Matt\nDrawing classes",
+      subtitle: "Classes will resume from 20th August 2021",
+      imgUrl:
+          "https://images.unsplash.com/photo-1627346980717-02226f9ff7f6?ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDE4fHRvd0paRnNrcEdnfHxlbnwwfHx8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+      date: "6:12 PM",
+      isStarred: true),
+  Mail(
+    title: "Matt\nDrawing classes",
+    subtitle: "Classes will resume from 20th August 2021",
+    imgUrl:
+        "https://images.unsplash.com/photo-1627346980717-02226f9ff7f6?ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDE4fHRvd0paRnNrcEdnfHxlbnwwfHx8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+    date: "6:12 PM",
+  ),
+  Mail(
+    title: "Matt\nDrawing classes",
+    subtitle: "Classes will resume from 20th August 2021",
+    imgUrl:
+        "https://images.unsplash.com/photo-1627346980717-02226f9ff7f6?ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDE4fHRvd0paRnNrcEdnfHxlbnwwfHx8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+    date: "6:12 PM",
+  ),
+];
